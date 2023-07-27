47a12900a1ff20f2c6bf6762aec117ac59288a09
Nikita Gorelov <gorelovnikita93@gmail.com>
2023-07-27 17:57:57 +0400

    add footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 import { Lang } from './share/models/lang';
 
 export function createTranslateLoader(http: HttpClient) {
@@ -18,7 +19,7 @@ export function createTranslateLoader(http: HttpClient) {
 }
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
