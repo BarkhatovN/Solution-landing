25137b525531364f6ded54661eeb6f2c593c64b8
Nikita Gorelov <gorelovnikita93@gmail.com>
2023-07-27 17:46:44 +0400

    init ngx-translator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,44 @@
 import { NgModule } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
+
+import {
+  TranslateLoader,
+  TranslateModule,
+  TranslateService,
+} from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { Lang } from './share/models/lang';
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent
-  ],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: createTranslateLoader,
+        deps: [HttpClient],
+      },
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+  constructor(translate: TranslateService) {
+    // this language will be used as a fallback when a translation isn't found in the current language
+    translate.setDefaultLang(Lang.EN);
+
+    // the lang to use, if the lang isn't available, it will use the current loader to get them
+    translate.use(Lang.EN);
+  }
+}
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { Lang } from '../share/models/lang';
 
 @Component({
   selector: 'solution-header',
@@ -7,5 +9,19 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent {
-  toggleLang(): void {}
+  readonly Lang = Lang;
+
+  langButtonText: Lang;
+
+  constructor(private readonly translateService: TranslateService) {
+    this.langButtonText =
+      (this.translateService.currentLang as Lang) === Lang.EN
+        ? Lang.RU
+        : Lang.EN;
+  }
+
+  toggleLang(): void {
+    this.translateService.use(this.langButtonText);
+    this.langButtonText = this.langButtonText === Lang.EN ? Lang.RU : Lang.EN;
+  }
 }
